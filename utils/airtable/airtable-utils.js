9670eff06e29638ab36fd0e0541a3fba4b2adcca
Marianna Di Vito <maridivi89@gmail.com>
2024-02-25 17:25:11 +0000

    Fix issues with products rendering

--- utils/airtable/airtable-utils.js
+++ utils/airtable/airtable-utils.js
@@ -1,6 +1,6 @@
 import Airtable from "airtable";
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_ID
-);
+const base = new Airtable({
+  apiKey: process.env.TOKEN,
+}).base(process.env.AIRTABLE_BASE_ID);
 
 export const table = base(process.env.AIRTABLE_TABLE_NAME);
